a7565ea91d184db31f05cfb8d1dc2066bf32411b
valkut22 <valkut88@gmail.com>
2024-09-29 13:18:43 +0200

    Update blume.user.js

--- blume.user.js
+++ blume.user.js
@@ -430,7 +430,7 @@ try {
         } else if (continueButton) {
           setTimeout(() => {
             continueButton.click();
-          }, 5000);
+          }, 7000);
         }
       }, Math.floor(Math.random() * 5000) + 5000);
     }
